66d60bb4e4f64b1e7a8bd34cc69f8e6c99e1ee45
RabailHumayun <122204934+RabailHumayun@users.noreply.github.com>
2024-09-09 20:54:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var button = document.getElementById('btn');
+var input1 = document.getElementById('input1');
+var input2 = document.getElementById('input2');
+
+button.addEventListener('click', function() {
+document.write(  input1.value  );
+document.write(  input2.value  );
+}, false);
